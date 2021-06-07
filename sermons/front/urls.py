469d6a1cbfc8d47f10f2e638a638d4339c55451b
godfrey-asiimwe <agtumusiime@gmail.com>
2021-06-07 13:00:56 +0300

    First Commit

---
+++ sermons/front/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.listing, name="listing"),
+    path("view_sermon/<int:sermon_id>/", views.view_sermon, name="view_sermon"),
+    path("see_request/", views.see_request),
+    path("user_info/", views.user_info),
+    path("private_place/", views.private_place),
+    path("staff_place/", views.staff_place),
+]
---
+++ sermons/front/views.py
@@ -0,0 +1,57 @@
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from .models import Sermon
+
+def see_request(request):
+    text = f"""
+        Some attributes of the HttpRequest object:
+
+        scheme: {request.scheme}
+        path:   {request.path}
+        method: {request.method}
+        GET:    {request.GET}
+        user:   {request.user}
+    """
+
+    return HttpResponse(text, content_type="text/plain")
+
+def user_info(request):
+    text = f"""
+        Selected HttpRequest.user attributes:
+
+        username:     {request.user.username}
+        is_anonymous: {request.user.is_anonymous}
+        is_staff:     {request.user.is_staff}
+        is_superuser: {request.user.is_superuser}
+        is_active:    {request.user.is_active}
+    """
+
+    return HttpResponse(text, content_type="text/plain")
+
+@login_required
+def private_place(request):
+    return HttpResponse("Shhh, members only!", content_type="text/plain")
+
+
+def listing(request):
+    data = {
+        "sermons": Sermon.objects.all(),
+    }
+
+    return render(request, "listing.html", data)
+
+def view_sermon(request, sermon_id):
+    sermon = get_object_or_404(Sermon, id=sermon_id)
+    data = {
+        "sermons": sermon,
+    }
+
+    return render(request, "view_sermon.html", data)
+
+@user_passes_test(lambda user: user.is_staff)
+def staff_place(request):
+    return HttpResponse("Employees must wash hands", content_type="text/plain")
+
